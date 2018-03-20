96d28d38c445b06c24cba80f716d715ed0a5b434
Brian Demers <bdemers@apache.org>
2018-03-20 14:31:41 -0400

    initial code commit

---
+++ creds-example-client/src/main/java/com/example/credsexampleclient/ClientApplication.java
@@ -0,0 +1,41 @@
+package com.example.credsexampleclient;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+
+@SpringBootApplication
+public class ClientApplication implements CommandLineRunner {
+
+    private final Logger logger = LoggerFactory.getLogger(ClientApplication.class);
+
+    @Value("#{ @environment['example.baseUrl'] }")
+    private String serverBaseUrl;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+    @Bean
+    @ConfigurationProperties("example.oauth2.client")
+    protected ClientCredentialsResourceDetails oAuthDetails() {
+        return new ClientCredentialsResourceDetails();
+    }
+
+    @Bean
+    protected OAuth2RestTemplate restTemplate() {
+        return new OAuth2RestTemplate(oAuthDetails());
+    }
+
+    @Override
+    public void run(String... args) {
+        logger.info("MOD: {}", restTemplate().getForObject(serverBaseUrl + "/mod", String.class));
+    }
+}
